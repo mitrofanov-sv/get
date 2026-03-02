354ff1fc6b52170b65774d337011101d967df1e2
Митрофанов Сергей <big_p1g@mail.ru>
2026-03-02 12:03:49 +0300

    Update pwm-led.py

--- get-led/pwm-led.py
+++ get-led/pwm-led.py
@@ -14,3 +14,36 @@ while True:
     duty += 1.0
     if duty > 100.0:
         duty=0.0
+
+import time
+from adc_plot import plot_voltage_vs_time
+from r2r_adc import R2R_ADC 
+
+DYNAMIC_RANGE = 3.3 
+COMPARE_TIME = 0.0001 
+DURATION = 3.0  
+
+def main():
+    adc = R2R_ADC(dynamic_range=DYNAMIC_RANGE, 
+                  compare_time=COMPARE_TIME, 
+                  verbose=False)
+    
+    voltage_values = []
+    time_values = []
+    
+    try:
+        start_time = time.time()
+        while time.time() - start_time < DURATION:
+            voltage = adc.get_sc_voltage()
+            current_time = time.time() - start_time
+            
+            voltage_values.append(voltage)
+            time_values.append(current_time)
+
+        plot_voltage_vs_time(time_values, voltage_values, DYNAMIC_RANGE)
+        
+    finally:
+        del adc
+
+if __name__ == "__main__":
+    main()
